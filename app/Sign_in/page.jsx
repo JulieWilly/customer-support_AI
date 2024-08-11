f44ea1ce01fce8025ca6b6546c0a782ff03c5f43
Wilfred Muchire <logincoin40@gmail.com>
2024-08-11 22:18:48 +0300

    adding firebase and auth functionality

--- app/Sign_in/page.jsx
+++ app/Sign_in/page.jsx
@@ -7,8 +7,10 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { FaGripLines } from "react-icons/fa";
-
+import auth from '@/config';
+import { GoogleAuthProvider, FacebookAuthProvider, TwitterAuthProvider, GithubAuthProvider, signInWithPopup } from 'firebase/auth';
 const page = () => {
+
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
@@ -16,6 +18,51 @@ const login = () => {
 
 }
 
+const signInWithGoogle = async () => {
+    try{
+        const provider = new GoogleAuthProvider();
+        const result = signInWithPopup(auth, provider)
+
+        console.log(result)
+
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+const signInWithFacebook= async () => {
+  try {
+    const provider = new FacebookAuthProvider();
+    const result = signInWithPopup(auth, provider);
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+const signInWithTwitter = async () => {
+  try {
+    const provider = new TwitterAuthProvider();
+    const result = signInWithPopup(auth, provider);
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const signInWithGithub = async () => {
+  try {
+    const provider = new GithubAuthProvider();
+    const result = signInWithPopup(auth, provider);
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
     return (
       <div className="signUpSect">
         <h1>HeadStarter Mental Care Support</h1>
@@ -23,9 +70,19 @@ const login = () => {
         <p>Sign in to proceed.</p>
         <div className="sign_up">
           <form action="">
-            <input type="email" value={email}  onChange={(e) => setEmail(e.target.value)} placeholder="Email address" />
-            <input type="number" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Phone number" />
-            <p className='forgotPassword'>Forgot Password?</p>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email address"
+            />
+            <input
+              type="number"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Phone number"
+            />
+            <p className="forgotPassword">Forgot Password?</p>
             <button onClick={login()}>Sign up</button>
             <div className="lines">
               {<FaGripLines className="length" />} Or{" "}
@@ -33,10 +90,18 @@ const login = () => {
             </div>
             <p>Sign in with</p>
             <div className="socials">
-              <div className="account">{<FaGoogle />} Google</div>
-              <div className="account">{<FaFacebookF />} Facebook</div>
-              <div className="account">{<FaXTwitter />} Twitter</div>
-              <div className="account">{<FaGithub />} Github</div>
+              <div className="account" onClick={signInWithGoogle}>
+                {<FaGoogle />} Google
+              </div>
+              <div className="account" onClick={signInWithFacebook}>
+                {<FaFacebookF />} Facebook
+              </div>
+              <div className="account" onClick={signInWithTwitter}>
+                {<FaXTwitter />} Twitter
+              </div>
+              <div className="account" onClick={signInWithGithub}>
+                {<FaGithub />} Github
+              </div>
             </div>
           </form>
         </div>
--- app/Sign_up/page.jsx
+++ app/Sign_up/page.jsx
@@ -2,13 +2,22 @@
 import { TextField } from '@mui/material';
 import React, { useState } from 'react';
 import './auth.css'
-import Link from 'next/link';
+import auth from '@/config';
+import { GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider, TwitterAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const login = () => {
     const [details, setDetails] = useState()
     const handleSignUp = () => {
-
+    singInWithGoogle;
     }
+
+  // sing in with google.
+  const singInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider()
+    const result = await signInWithPopup(auth, provider)
+
+    console.log(result)
+  }
     return (
       <div className="signUpSect">
         <h1>HeadStarter Mental Care Support</h1>
